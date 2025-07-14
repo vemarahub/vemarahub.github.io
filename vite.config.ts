fcc0b7a30ca7a4146a80410153ce39556be394ac
Rajesh S Nair <67047853+vemarahub@users.noreply.github.com>
2025-07-14 21:28:20 +0000

    changing base to /

--- vite.config.ts
+++ vite.config.ts
@@ -17,5 +17,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: mode === 'production' ? '/vemarahub.github.io/' : '/',
+base: '/',
 }));
