0054fd5211824090d377e050e12494ba2a31a922
Rajesh S Nair <67047853+vemarahub@users.noreply.github.com>
2025-07-15 15:21:09 +0200

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -31,7 +31,7 @@ const HeroSection = () => {
 <img 
   src={Logo} 
   alt="Hello Wereld Logo" 
-  className="w-64 sm:w-72 lg:w-80 mb-6 mx-auto"
+  className="w-full max-w-[500px] mb-6 mx-auto"
 />
 
 
